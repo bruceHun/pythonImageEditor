03fb63fd79558e6bfda99c0195dd65a4827a76a4
BruceHun <bhung2011@gmail.com>
2020-12-09 00:35:32 +0800

    Tmp update

--- viaorganizer_app.py
+++ viaorganizer_app.py
@@ -1,9 +1,9 @@
-import os
 
 from PyQt5.QtGui import QPixmap, QPolygon, QPen, QPainterPath, QPolygonF, QWheelEvent, QTransform, QKeyEvent, QColor, \
-    QKeySequence
-from PyQt5.QtWidgets import QFileDialog, QGraphicsPathItem, QTableWidgetItem, QTableWidget, QHeaderView
-from PyQt5.QtCore import Qt, QPoint, QPointF
+    QKeySequence, QDropEvent
+from PyQt5.QtWidgets import QFileDialog, QGraphicsPathItem, QTableWidgetItem, QTableWidget, QHeaderView, QMessageBox, \
+    QListWidget
+from PyQt5.QtCore import Qt, QPoint, QPointF, pyqtSignal, QObject
 
 from viaorganizer import Ui_MainWindow
 from anno_info import Ui_Form
@@ -66,8 +66,8 @@ class ViaOrganizer:
             with open(f_name[i], 'r') as json_file:
                 self.json_in[i] = json.load(json_file)
             print(len(self.json_in[i]))
-        for i in range(2):
-            sub = QtWidgets.QMdiSubWindow()
+        for i in [1, 0]:
+            sub = QtWidgets.QMdiSubWindow(flags=Qt.CustomizeWindowHint)
             form = QtWidgets.QWidget()
             self.Form[i]: Ui_Form = Ui_Form()
             self.Form[i].setupUi(form)
@@ -86,6 +86,13 @@ class ViaOrganizer:
             if self.Form[i].listWidget.count() > 0:
                 self.Form[i].listWidget.setCurrentRow(0)
 
+        f1: Ui_Form = self.Form[0]
+        f2: Ui_Form = self.Form[1]
+        f2.listWidget.setDragDropMode(QtWidgets.QAbstractItemView.DragOnly)
+        f2.listWidget.setDefaultDropAction(Qt.CopyAction)
+        f2.listWidget_2.setDragDropMode(QtWidgets.QAbstractItemView.DragOnly)
+        f2.listWidget_2.setDefaultDropAction(Qt.CopyAction)
+
         self.scene = QtWidgets.QGraphicsScene()
         self.UI.graphicsView.setScene(self.scene)
 
@@ -93,6 +100,8 @@ class ViaOrganizer:
 
         self.UI.centralwidget.keyPressEvent = self.key_event
         self.UI.graphicsView.wheelEvent = self.wheel_event
+        f1.listWidget.dropEvent = self.drop_annotation_src_to_des
+        f1.listWidget_2.dropEvent = self.drop_region_src_to_des
         # self.Form[0].listWidget_2.mousePressEvent = self.click_on_dest
         # self.Form[1].listWidget_2.mousePressEvent = self.click_on_src
 
@@ -103,6 +112,7 @@ class ViaOrganizer:
         self.UI.actionConvert_to_VIA_naming.triggered.connect(self.convert_to_VIA_naming)
         self.Form[0].listWidget.currentRowChanged.connect(lambda: self.update_preview(0))
         self.Form[0].listWidget_2.currentRowChanged.connect(lambda: self.show_attribute(0))
+        self.Form[0].tableWidget.cellChanged.connect(self.edit_attribute)
         self.Form[1].listWidget.currentRowChanged.connect(lambda: self.update_preview(1))
         self.Form[1].listWidget_2.currentRowChanged.connect(lambda: self.show_attribute(1))
 
@@ -164,12 +174,32 @@ class ViaOrganizer:
         table.setRowCount(len(r))
         table.setColumnCount(2)
         table.horizontalHeader().setSectionResizeMode(QHeaderView.ResizeToContents)
-
+        # self.Form[idx].listWidget_2.connect(table, pyqtSignal("itemClicked(QTableWidgetItem *)"), self.edit_attribute)
         for i, attr in enumerate(r):
             # self.Form[idx].listWidget_3.addItem(f"{attr}: {r[attr]}")
             table.setItem(i, 0, QTableWidgetItem(attr))
             table.setItem(i, 1, QTableWidgetItem(r[attr]))
         table.show()
+
+    def edit_attribute(self, i, j):
+        print(f'cell {i, j} changes')
+        f1: Ui_Form = self.Form[0]
+        a = f1.listWidget.currentItem().text()
+        r = self.json_in[0][a]['regions']
+        idx = f1.listWidget_2.currentRow()
+        attr = r[idx]['region_attributes']
+        try:
+            if j > 0:
+                key = list(attr.keys())[i]
+                attr[key] = f1.tableWidget.item(i, j).text()
+            else:
+                key = list(attr.keys())[i]
+                del attr[key]
+                val = list(attr.values())[j]
+                attr[f1.tableWidget.item(i, j).text()] = val
+            # f1.tableWidget.cellChanged.connect(lambda: print('do nothing'))
+        except IndexError:
+            return
 
     def key_event(self, e: QKeyEvent):
         key = e.key()
@@ -187,18 +217,45 @@ class ViaOrganizer:
             delta /= 10
             self.zoom_preview(delta.y())
 
+    def drop_annotation_src_to_des(self, e: QDropEvent):
+        f1: Ui_Form = self.Form[0]
+        f2: Ui_Form = self.Form[1]
+        src: QListWidget = e.source()
+        in_text = src.currentItem().text()
+        if e.source() is f2.listWidget:
+            if len(f1.listWidget.findItems(in_text, Qt.MatchExactly)) > 0:
+                result = QMessageBox.question(self.UI.centralwidget,
+                                              "Override annotation?",
+                                              "Annotations for this image already exists.\n"
+                                              "Are your sure you want to override them?\n"
+                                              "\n[Yes] saves your changes"
+                                              "\n[No]  discards your changes",
+                                              QMessageBox.Yes | QMessageBox.No)
+                if result == QMessageBox.No:
+                    return
+
+                self.json_in[0][in_text] = self.json_in[1][in_text]
+                print('drop accepted')
+                self.update_preview()
+        else:
+            print('drop denied')
+
+    def drop_region_src_to_des(self, e: QDropEvent):
+        print('dropped')
+        f1: Ui_Form = self.Form[0]
+        f2: Ui_Form = self.Form[1]
+        src: QListWidget = e.source()
+        src_text = f2.listWidget.currentItem().text()
+        des_text = f1.listWidget.currentItem().text()
+        self.json_in[0][des_text]['regions'].append(self.json_in[1][src_text]['regions'][src.currentRow()])
+        self.update_preview(0)
+
     def zoom_preview(self, delta_y: float):
         m: QTransform = QTransform()
         self.zoom = max(min(self.zoom + delta_y, 10), 0.01)
         m.scale(self.zoom, self.zoom)
         # print(delta_y, self.zoom)
         self.UI.graphicsView.setTransform(m)
-
-    def click_on_dest(self, e):
-        print('click_on_destination')
-
-    def click_on_src(self, e):
-        print('click_on_source')
 
     # 另存異動後 JSON 檔案
     def save_file(self):
