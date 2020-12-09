e4f6a49f0459827d84f88a06fa8f22a2c1fda8db
BHung <bhung2011@gmail.com>
2020-12-09 10:42:37 +0800

    Update viaorganizer

--- viaorganizer_app.py
+++ viaorganizer_app.py
@@ -1,4 +1,3 @@
-
 from PyQt5.QtGui import QPixmap, QPolygon, QPen, QPainterPath, QPolygonF, QWheelEvent, QTransform, QKeyEvent, QColor, \
     QKeySequence, QDropEvent
 from PyQt5.QtWidgets import QFileDialog, QGraphicsPathItem, QTableWidgetItem, QTableWidget, QHeaderView, QMessageBox, \
@@ -9,7 +8,8 @@ from viaorganizer import Ui_MainWindow
 from anno_info import Ui_Form
 from PyQt5 import QtWidgets
 from typing import Union
-from configparser import ConfigParser, NoOptionError
+from configparser import ConfigParser, NoOptionError, NoSectionError
+from copy import deepcopy
 import json
 
 colors = {
@@ -36,6 +36,8 @@ class ViaOrganizer:
         self.config: Union[ConfigParser, None] = None
         self.zoom: float = 1.0
         self.on_form: int = 1
+        self.loading_attribute: bool = False
+        self.curr_idx: list = [0, 0]
 
     def init(self):
         self.config = ConfigParser()
@@ -45,9 +47,10 @@ class ViaOrganizer:
             settings.close()
             self.config.read("viaorg_settings.ini")
             self.directory = self.config.get("GeneralSettings", 'ImageDir')
-            f_name = [self.config.get("GeneralSettings", 'Source'), self.config.get("GeneralSettings", 'Destination')]
-
-        except (FileNotFoundError, NoOptionError):
+            f_name = [self.config.get("GeneralSettings", 'Destination'), self.config.get("GeneralSettings", 'Source')]
+            self.curr_idx[0] = int(self.config.get("WorkingState", "SrcIndex"))
+            self.curr_idx[1] = int(self.config.get("WorkingState", "DesIndex"))
+        except (FileNotFoundError, NoOptionError, NoSectionError):
             # 指定圖片資料夾
             self.directory = str(QFileDialog.getExistingDirectory(None, "Select Directory"))
             # 開 JSON 檔
@@ -56,9 +59,12 @@ class ViaOrganizer:
             f_name = [res1, res2]
 
             self.config["GeneralSettings"] = {'ImageDir': self.directory,  # 圖片資料夾
-                                              'Source': f_name[0],
-                                              'Destination': f_name[1]
+                                              'Destination': f_name[0],
+                                              'Source': f_name[1]
                                               }
+            self.config["WorkingState"] = {'SrcIndex': "0",
+                                           'DesIndex': "0"
+                                           }
             with open('viaorg_settings.ini', 'w') as file:
                 self.config.write(file)
 
@@ -84,7 +90,7 @@ class ViaOrganizer:
                 # self.UI.listWidget.addItem(filename)
                 self.Form[i].listWidget.addItem(filename)
             if self.Form[i].listWidget.count() > 0:
-                self.Form[i].listWidget.setCurrentRow(0)
+                self.Form[i].listWidget.setCurrentRow(self.curr_idx[i])
 
         f1: Ui_Form = self.Form[0]
         f2: Ui_Form = self.Form[1]
@@ -98,49 +104,60 @@ class ViaOrganizer:
 
         self.update_preview(0)
 
-        self.UI.centralwidget.keyPressEvent = self.key_event
         self.UI.graphicsView.wheelEvent = self.wheel_event
+        self.win.closeEvent = self.on_exit
         f1.listWidget.dropEvent = self.drop_annotation_src_to_des
         f1.listWidget_2.dropEvent = self.drop_region_src_to_des
+        f1.listWidget.keyPressEvent = self.f1_anno_key_event
+        f1.listWidget_2.keyPressEvent = self.f1_region_key_event
         # self.Form[0].listWidget_2.mousePressEvent = self.click_on_dest
         # self.Form[1].listWidget_2.mousePressEvent = self.click_on_src
 
         # UI 功能連結
         self.UI.action_Save.triggered.connect(self.save_file)
-        self.UI.action_Delete.triggered.connect(self.delete_selected_rows)
-        self.UI.actionConvert_to_simple_name.triggered.connect(self.convert_to_simple_naming)
+        self.UI.action_Delete.triggered.connect(self.delete_selected_annotations)
+        self.UI.actionZoom_In.triggered.connect(lambda: self.zoom_preview(0.1))
+        self.UI.actionZoom_Out.triggered.connect(lambda: self.zoom_preview(-0.1))
+        # self.UI.actionConvert_to_simple_name.triggered.connect(self.convert_to_simple_naming)
         self.UI.actionConvert_to_VIA_naming.triggered.connect(self.convert_to_VIA_naming)
         self.Form[0].listWidget.currentRowChanged.connect(lambda: self.update_preview(0))
+        self.Form[0].listWidget.itemClicked.connect(lambda: self.update_preview(0))
         self.Form[0].listWidget_2.currentRowChanged.connect(lambda: self.show_attribute(0))
+        self.Form[0].listWidget_2.itemClicked.connect(lambda: self.show_attribute(0))
         self.Form[0].tableWidget.cellChanged.connect(self.edit_attribute)
         self.Form[1].listWidget.currentRowChanged.connect(lambda: self.update_preview(1))
+        self.Form[1].listWidget.itemClicked.connect(lambda: self.update_preview(1))
         self.Form[1].listWidget_2.currentRowChanged.connect(lambda: self.show_attribute(1))
+        self.Form[1].listWidget_2.itemClicked.connect(lambda: self.show_attribute(1))
 
     # 更新預覽
     def update_preview(self, idx: int):
         self.on_form = idx
+        form = self.Form[idx]
         curr_text = self.Form[idx].listWidget.currentItem().text()
         f_name = f"{self.directory}/{self.json_in[idx][curr_text]['filename']}"
         if self.FormPreview is None:
             self.FormPreview = self.scene.addPixmap(QPixmap(f_name))
         else:
             self.FormPreview.setPixmap(QPixmap(f_name))
-        self.UI.graphicsView.fitInView(self.FormPreview, Qt.KeepAspectRatio)
-        self.zoom = self.UI.graphicsView.viewportTransform().m11()
 
         # Populate ListWidget
-        self.Form[idx].listWidget_2.clear()
+        form.listWidget_2.clear()
         # self.Form[idx].listWidget_3.clear()
-        self.Form[idx].tableWidget.clear()
+        form.tableWidget.clear()
         for i, region in enumerate(self.json_in[idx][curr_text]['regions']):
             # print(i, region['region_attributes'])
             # self.UI.listWidget.addItem(filename)
             self.Form[idx].listWidget_2.addItem(str(i))
+        form.listWidget_2.setCurrentRow(0)
+
+        self.UI.graphicsView.fitInView(self.FormPreview, Qt.KeepAspectRatio)
+        self.zoom = self.UI.graphicsView.viewportTransform().m11()
 
         for mk in self.mark:
             self.scene.removeItem(mk)
         self.mark.clear()
-        for curr_idx in range(self.Form[idx].listWidget_2.count()):
+        for curr_idx in range(form.listWidget_2.count()):
             area: QPolygonF = QPolygonF()
             p = self.json_in[idx][curr_text]['regions'][curr_idx]['shape_attributes']
             for i in range(len(p['all_points_x'])):
@@ -175,38 +192,13 @@ class ViaOrganizer:
         table.setColumnCount(2)
         table.horizontalHeader().setSectionResizeMode(QHeaderView.ResizeToContents)
         # self.Form[idx].listWidget_2.connect(table, pyqtSignal("itemClicked(QTableWidgetItem *)"), self.edit_attribute)
+        self.loading_attribute = True
         for i, attr in enumerate(r):
             # self.Form[idx].listWidget_3.addItem(f"{attr}: {r[attr]}")
             table.setItem(i, 0, QTableWidgetItem(attr))
             table.setItem(i, 1, QTableWidgetItem(r[attr]))
+        self.loading_attribute = False
         table.show()
-
-    def edit_attribute(self, i, j):
-        print(f'cell {i, j} changes')
-        f1: Ui_Form = self.Form[0]
-        a = f1.listWidget.currentItem().text()
-        r = self.json_in[0][a]['regions']
-        idx = f1.listWidget_2.currentRow()
-        attr = r[idx]['region_attributes']
-        try:
-            if j > 0:
-                key = list(attr.keys())[i]
-                attr[key] = f1.tableWidget.item(i, j).text()
-            else:
-                key = list(attr.keys())[i]
-                del attr[key]
-                val = list(attr.values())[j]
-                attr[f1.tableWidget.item(i, j).text()] = val
-            # f1.tableWidget.cellChanged.connect(lambda: print('do nothing'))
-        except IndexError:
-            return
-
-    def key_event(self, e: QKeyEvent):
-        key = e.key()
-        if key == Qt.Key_Plus or key == Qt.Key_Equal:
-            self.zoom_preview(0.1)
-        elif key == Qt.Key_Minus:
-            self.zoom_preview(-0.1)
 
     def wheel_event(self, e: QWheelEvent):
         keyname = QKeySequence(e.modifiers()).toString()
@@ -234,21 +226,39 @@ class ViaOrganizer:
                 if result == QMessageBox.No:
                     return
 
-                self.json_in[0][in_text] = self.json_in[1][in_text]
+                self.json_in[0][in_text] = deepcopy(self.json_in[1][in_text])
                 print('drop accepted')
-                self.update_preview()
+                self.update_preview(0)
         else:
             print('drop denied')
 
     def drop_region_src_to_des(self, e: QDropEvent):
-        print('dropped')
         f1: Ui_Form = self.Form[0]
         f2: Ui_Form = self.Form[1]
         src: QListWidget = e.source()
         src_text = f2.listWidget.currentItem().text()
         des_text = f1.listWidget.currentItem().text()
-        self.json_in[0][des_text]['regions'].append(self.json_in[1][src_text]['regions'][src.currentRow()])
+        new_data = deepcopy(self.json_in[1][src_text]['regions'][src.currentRow()])
+        self.json_in[0][des_text]['regions'].append(new_data)
         self.update_preview(0)
+
+    def edit_attribute(self, i, j):
+        if self.loading_attribute:
+            return
+        f1: Ui_Form = self.Form[0]
+        a = f1.listWidget.currentItem().text()
+        r = self.json_in[0][a]['regions']
+        idx = f1.listWidget_2.currentRow()
+        attr: dict = r[idx]['region_attributes']
+        keys = list(attr.keys())
+        for idx, key in enumerate(keys):
+            if j > 0 and i == idx:
+                attr[key] = f1.tableWidget.item(i, j).text()
+            elif j == 0 and i == idx:
+                attr[f1.tableWidget.item(i, j).text()] = attr.pop(key)
+            else:
+                tmp = attr.pop(key)
+                attr[key] = tmp
 
     def zoom_preview(self, delta_y: float):
         m: QTransform = QTransform()
@@ -257,39 +267,75 @@ class ViaOrganizer:
         # print(delta_y, self.zoom)
         self.UI.graphicsView.setTransform(m)
 
+    def f1_anno_key_event(self, e: QKeyEvent):
+        # super(QListWidget, self.Form[0].listWidget).keyPressEvent(e)
+        key = e.key()
+        if key == Qt.Key_Delete or key == Qt.Key_Backspace:
+            self.delete_selected_annotations()
+
+    # 目標資料 region 欄位鍵盤事件
+    def f1_region_key_event(self, e: QKeyEvent):
+        key = e.key()
+        if key == Qt.Key_Delete or key == Qt.Key_Backspace:
+            self.delete_selected_regions()
+        elif key == Qt.Key_Up:
+            f1: Ui_Form = self.Form[0]
+            f1.listWidget_2.setCurrentRow(max(f1.listWidget_2.currentRow() - 1, 0))
+        elif key == Qt.Key_Down:
+            f1: Ui_Form = self.Form[0]
+            end = f1.listWidget_2.count() - 1
+            f1.listWidget_2.setCurrentRow(min(f1.listWidget_2.currentRow() + 1, end))
+
     # 另存異動後 JSON 檔案
     def save_file(self):
-        json_out = {}
-        for i in range(self.UI.listWidget.count()):
-            filename = self.UI.listWidget.item(i).text()
-            json_out[filename] = self.json_in[filename]
+        result = QMessageBox.question(self.UI.centralwidget,
+                                      "Save changes?",
+                                      "Would you like to save your changes?\n"
+                                      "Note: This action cannot be undone.",
+                                      QMessageBox.Yes | QMessageBox.No)
 
-        with open('../modified_json.json', 'w') as json_file:
-            json_file.write(str(json.dumps(json_out)))
+        if result == QMessageBox.Yes:
+            json_out = {}
+            for i in range(self.Form[0].listWidget.count()):
+                filename = self.Form[0].listWidget.item(i).text()
+                json_out[filename] = self.json_in[0][filename]
+
+            with open(self.config.get("GeneralSettings", 'Destination'), 'w') as json_file:
+                json_file.write(str(json.dumps(json_out)))
 
     # 刪除選取項目
-    def delete_selected_rows(self):
-        while len(self.UI.listWidget.selectedIndexes()) > 0:
-            selections = self.UI.listWidget.selectedIndexes()
-            self.UI.listWidget.takeItem(selections[0].row())
+    def delete_selected_annotations(self):
+        print('delete annotations')
+        while len(self.Form[0].listWidget.selectedIndexes()) > 0:
+            selections = self.Form[0].listWidget.selectedIndexes()
+            self.Form[0].listWidget.takeItem(selections[0].row())
+
+    def delete_selected_regions(self):
+        print('delete regions')
+        f1: Ui_Form = self.Form[0]
+        f1.listWidget_2.takeItem(f1.listWidget_2.currentRow())
+        r: list = self.json_in[0][f1.listWidget.currentItem().text()]['regions']
+        ridx = f1.listWidget_2.currentRow()
+        r.pop(ridx)
+        self.update_preview(0)
 
     def convert_to_simple_naming(self):
         json_out = {}
-        for i in range(self.UI.listWidget.count()):
-            in_f_name = self.UI.listWidget.item(i).text()
+        for i in range(self.Form[0].listWidget.count()):
+            in_f_name = self.Form[0].listWidget.item(i).text()
             out_f_name = self.json_in[in_f_name]['filename']
             json_out[out_f_name] = self.json_in[in_f_name]
 
         self.json_in = json_out
         # Re-Populate ListWidget
-        self.UI.listWidget.clear()
+        self.Form[0].listWidget.clear()
         for i, filename in enumerate(self.json_in):
-            self.UI.listWidget.addItem(filename)
+            self.Form[0].listWidget.addItem(filename)
 
     def convert_to_VIA_naming(self):
         json_out = {}
-        for i in range(self.UI.listWidget.count()):
-            in_f_name = self.UI.listWidget.item(i).text()
+        for i in range(self.Form[0].listWidget.count()):
+            in_f_name = self.Form[0].listWidget.item(i).text()
             out_f_name = self.json_in[in_f_name]['filename']
             f_size = self.json_in[in_f_name]['size']
             out_f_name = f'{out_f_name}{f_size}'
@@ -297,13 +343,21 @@ class ViaOrganizer:
 
         self.json_in = json_out
         # Re-Populate ListWidget
-        self.UI.listWidget.clear()
+        self.Form[0].listWidget.clear()
         for i, filename in enumerate(self.json_in):
-            self.UI.listWidget.addItem(filename)
+            self.Form[0].listWidget.addItem(filename)
+
+    def on_exit(self, e):
+        self.config["WorkingState"] = {'SrcIndex': str(self.Form[0].listWidget.currentRow()),
+                                       'DesIndex': str(self.Form[1].listWidget.currentRow())
+                                       }
+        with open('viaorg_settings.ini', 'w') as file:
+            self.config.write(file)
 
 
 if __name__ == "__main__":
     import sys
+
     app = QtWidgets.QApplication(sys.argv)
     MainWindow = QtWidgets.QMainWindow()
     ui = Ui_MainWindow()
